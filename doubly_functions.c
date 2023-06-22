b553a221f6f81435d9c753dc3315037baa8a9ef0
lewisthagichu <lewisthagichu254@gmail.com>
2023-06-22 21:50:37 +0300

    Initial commit

---
+++ doubly_functions.c
@@ -0,0 +1,90 @@
+#include "monty.h"
+
+/**
+ *add_dnodeint_end - adds a note at the end of the doubly link list
+ *@head: first position of linked list
+ *@n: data to be stored
+ *Return: a doubly linked list
+ */
+stack_t *add_dnodeint_end(stack_t **head, const int n)
+{
+	stack_t *temp, *aux;
+
+	if (head == NULL)
+		return (NULL);
+	temp = malloc(sizeof(stack_t));
+	if (!temp)
+	{
+		dprintf(2, "Error: malloc failed\n");
+		free_myGlobals();
+		exit(EXIT_FAILURE);
+	}
+	temp->n = n;
+	/*Careful */
+	if (*head == NULL)
+	{
+		temp->next = *head;
+		temp->prev = NULL;
+		*head = temp;
+		return (*head);
+	}
+	aux = *head;
+	while (aux->next)
+		aux = aux->next;
+	temp->next = aux->next;
+	temp->prev = aux;
+	aux->next = temp;
+	return (aux->next);
+}
+
+/**
+ *add_dnodeint - adds a note at the begining of the doubly link list
+ *@head: first position of linked list
+ *@n: data to store
+ *Return: a doubly linked list
+ */
+stack_t *add_dnodeint(stack_t **head, const int n)
+{
+	stack_t *temp;
+
+	if (head == NULL)
+		return (NULL);
+	temp = malloc(sizeof(stack_t));
+	if (!temp)
+	{
+		dprintf(2, "Error: malloc failed\n");
+		free_myGlobals();
+		exit(EXIT_FAILURE);
+	}
+	temp->n = n;
+	/*Careful*/
+	if (*head == NULL)
+	{
+		temp->next = *head;
+		temp->prev = NULL;
+		*head = temp;
+		return (*head);
+	}
+	(*head)->prev = temp;
+	temp->next = (*head);
+	temp->prev = NULL;
+	*head = temp;
+	return (*head);
+}
+
+/**
+ * free_dlistint - frees the doubly linked list
+ *
+ * @head: head of the list
+ * Return: no return
+ */
+void free_dlistint(stack_t *head)
+{
+	stack_t *currentNode;
+
+	while ((currentNode = head) != NULL)
+	{
+		head = head->next;
+		free(currentNode);
+	}
+}
